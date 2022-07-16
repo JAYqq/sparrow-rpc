d389dbde6a54b8bbd8cf5e7297c90f02b090aebc
chengweishen <chengweishen@trip.com>
2022-07-16 21:58:13 +0800

    update

---
+++ sparrow-rpc-client/src/main/java/com/sparrow/mason/client/proxy.java
@@ -0,0 +1,8 @@
+package com.sparrow.mason.client;
+
+/**
+ * @author chengwei_shen
+ * @date 2022/7/16 21:56
+ **/
+public class proxy {
+}
---
+++ sparrow-rpc-server/src/main/java/com/sparrow/mason/server/Server.java
@@ -0,0 +1,8 @@
+package com.sparrow.mason.server;
+
+/**
+ * @author chengwei_shen
+ * @date 2022/7/16 21:57
+ **/
+public class Server {
+}
